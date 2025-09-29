eb30991b5ec3d2291826d4e7cf4730d6798cb949
Aneesa Shahmand <aneesashahmand2@gmail.com>
2025-09-29 22:30:48 +0500

    Add files via upload

---
+++ src/assets/data.js
@@ -0,0 +1,43 @@
+export const data = [
+    {
+        question: "What is the correct file extension for JavaScript files?",
+        option1: ".java",
+        option2: ".js",
+        option3: ".javascript",
+        option4: ".jsx",
+        ans: 2,
+    },
+    {
+      question: "Which keyword is used to declare a variable in JavaScript?",
+        option1: "int",
+        option2: "let",
+        option3: "string",
+        option4: "define",
+        ans: 2,  
+    },
+    {
+        question: "What is the purpose of useState in React?",
+        option1: "To perform HTTP requests",
+        option2: "To define CSS styles",
+        option3: "To manage component state",
+        option4: "To create new components",
+        ans: 3,  
+    },
+    {
+       question: "What is JavaScript mainly used for?",
+        option1: "Adding interactivity",
+        option2: "Designing logos",
+        option3: "Storing data only",
+        option4: "Styling web pages",
+        ans: 1,    
+    },
+    {
+      question: "Which is not a javascript framework?",
+        option1: "React",
+        option2: "Node",
+        option3: "Express",
+        option4: "Django",
+        ans: 4,    
+    },
+];
+
